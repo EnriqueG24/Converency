32efec896a862101b41f6ed435a2a6b2ac11e871
Enrique Gongora <EGTech24@gmail.com>
2021-01-25 20:58:31 -0800

    Bitcoin Tab successfully converts the price the user enters in the textfield to whatever currency they have selected on the picker view.

--- Converency/Converency/Views/BitcoinView.swift
+++ Converency/Converency/Views/BitcoinView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct BitcoinView: View {
+    
+    @ObservedObject var bitcoinNetworkManager = BitcoinNetworkManager()
+    @State private var pickerSelection = 0
+    @State private var amount: String = ""
+    
+    var total: Double {
+        // Ensure that our array has at least 1 item to avoid index out of range errors
+        guard bitcoinNetworkManager.buyingPrice.count > 0 else { return 0 }
+        
+        let buyingPrice = bitcoinNetworkManager.buyingPrice[pickerSelection]
+        let doubleAmount = Double(amount) ?? 0.0
+        let totalAmount = buyingPrice * doubleAmount
+        return totalAmount
+    }
+    
+    var symbol: String {
+        // Ensure that our array has at least 1 item to avoid index out of range errors
+        guard bitcoinNetworkManager.symbolArray.count > 0 else { return "" }
+        let currencySymbol = bitcoinNetworkManager.symbolArray[pickerSelection]
+        return currencySymbol
+    }
+    
     var body: some View {
-        Text("This is the bitcoin view")
+        VStack {
+            Spacer()
+            Text("\(symbol)\(total, specifier: "%.2f")")
+                .font(.system(size: 30))
+            
+            Spacer()
+            
+            TextField("Enter Amount:", text: $amount)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.decimalPad)
+                .padding()
+            Picker("", selection: $pickerSelection) {
+                ForEach(0..<bitcoinNetworkManager.currencyCode.count) {
+                    let currency = bitcoinNetworkManager.currencyCode[$0]
+                    Text(currency)
+                }
+            }
+            .id(UUID())
+            .labelsHidden()
+        }.padding()
     }
 }
 
